b001773ba359de48ba2a9f63dca47f093d1f67e7
haris <harishammadpak1@gmail.com>
2025-04-13 14:40:07 +1400

    9

--- app.js
+++ app.js
@@ -1,36 +1,47 @@
 document.querySelector('.dropdown-toggle').addEventListener('click', function () {
-    this.classList.toggle('active');
-  });
+  this.classList.toggle('active');
+});
+
 
 
 function setupMenu() {
-    const menuToggleBtn = document.querySelector(".menuToggleBtn");
-    const mobileMenu = document.querySelector(".mobileDropdownMenu");
-    const closeBtn = document.querySelector(".closeBtn");
-    const dropdownToggle = document.querySelector(".mobileDropdownMenu .dropdown-toggle");
+  const menuToggleBtn = document.querySelector(".menuToggleBtn");
+  const mobileMenu = document.querySelector(".mobileDropdownMenu");
+  const closeBtn = document.querySelector(".closeBtn");
+  const dropdownToggle = document.querySelector(".mobileDropdownMenu .dropdown-toggle");
+  const contact1 = document.querySelector(".contact1");
+
+  menuToggleBtn.addEventListener("click", () => {
+    mobileMenu.style.display = "flex";
+  });
+
+  closeBtn.addEventListener("click", () => {
+    mobileMenu.style.display = "none";
+  });
+
+  dropdownToggle.addEventListener("click", (e) => {
+    e.stopPropagation();
+    dropdownToggle.classList.toggle("active");
+
+
   
-    menuToggleBtn.addEventListener("click", () => {
-      mobileMenu.style.display = "flex";
-    });
-  
-    closeBtn.addEventListener("click", () => {
+    if (dropdownToggle.classList.contains("active")) {
+      contact1.classList.add("slide-down");
+    } else {
+      contact1.classList.remove("slide-down");
+    }
+    
+  });
+
+  document.addEventListener("click", (e) => {
+    if (!mobileMenu.contains(e.target) && !menuToggleBtn.contains(e.target)) {
       mobileMenu.style.display = "none";
-    });
-  
-    dropdownToggle.addEventListener("click", (e) => {
-      e.stopPropagation();
-      dropdownToggle.classList.toggle("active");
-    });
-  
-    document.addEventListener("click", (e) => {
-      if (!mobileMenu.contains(e.target) && !menuToggleBtn.contains(e.target)) {
-        mobileMenu.style.display = "none";
-        dropdownToggle.classList.remove("active");
-      }
-    });
-  }
-  
-  setupMenu();
+    }
+  });
+}
+
+setupMenu();
+
 
 
   function scrollSliderLeft() {
@@ -50,8 +61,19 @@ function setupMenu() {
   
   function scrollSliderRigh() {
     const container = document.getElementById("oye");
-    const scrollAmount = container.offsetWidth * 1.3; 
+    const scrollAmount = container.offsetWidth * 1.2; 
     container.scrollBy({ left: scrollAmount, behavior: "smooth" });
+  }
+  
+  function toggleAnswer(card) {
+    const answer = card.querySelector('.faq-answer');
+    const icon = card.querySelector('.faq-icon');
+  
+    // Toggle visibility
+    answer.style.display = (answer.style.display === 'block') ? 'none' : 'block';
+  
+    // Change icon
+    icon.textContent = (answer.style.display === 'block') ? '−' : '+';
   }
   
   
